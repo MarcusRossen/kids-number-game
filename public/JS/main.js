9d4ad8cbb96074afcb9d83e32765fd3f0656ec4d
Marcus Rossen <marcusrossen0102@gmail.com>
2024-11-01 12:57:12 +0100

    js structure changes

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,79 +1,86 @@
-const inputBoxes = document.getElementsByClassName('inputBox');
-const pointLabel = document.getElementById('pointsBox');
-var upperAnswer, lowerAnswer, rightAnswer, leftAnswer, upperInput, lowerInput, leftInput, rightInput;
-let points = "points = 0";
-var result = 0;
-let randomNumber;
+const pointScoreLabel = document.getElementById('pointsLabel');
+const highScoreLabel = document.getElementById('highScoreLabel');
+const randomNumberLabel = document.getElementById('randomNumberInputBox')
+const inputBoxes = Array.from(document.querySelectorAll('#topInputBox, #bottomInputBox, #rightInputBox, #leftInputBox'));
 
-document.getElementById('pointsBox').innerText = points;
-
-function getRandomNumber(max, min) {
-    return Math.floor(Math.random() * (max - min) + min);
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * ((max + 1) - min) + min);
 }
 
-function generateRandomNumbers() {
-    randomNumber = getRandomNumber(101, 11);
-    let randomNumberVariables = [getRandomNumber(11, 6), getRandomNumber(6, 1)];
+function populateGrid() {
+    const grid = document.getElementById('grid');
+    for (let i = 1; i <= 100; i++) {
+        const div = document.createElement('div');
+        div.classList.add('gridItem');
+        div.textContent = i;
+        grid.appendChild(div);
+    }
+}
 
-    document.getElementById('randomNumber').innerText = randomNumber;
-    document.getElementById('upperRandomNumber').innerText = randomNumberVariables[0];
-    document.getElementById('upperRandomNumberNegative').innerText = -randomNumberVariables[0];
-    document.getElementById('middleRandomNumber').innerText = randomNumberVariables[1];
-    document.getElementById('middleRandomNumberNegative').innerText = -randomNumberVariables[1];
+function gridColors(resetColors = false) {
+    let gridItems = Array.from(document.getElementsByClassName('gridItem'));
 
-    document.getElementById('upperInputBox').value = '';
-    document.getElementById('lowerInputBox').value = '';
-    document.getElementById('leftInputBox').value = '';
-    document.getElementById('rightInputBox').value = '';
+    if (resetColors) {
+        gridItems.forEach(gridItem => {
+            gridItem.style.backgroundColor = "white";
+        });
+        return;
+    }
 
-    upperAnswer = randomNumber + randomNumberVariables[0];
-    lowerAnswer = randomNumber - randomNumberVariables[0];
-    leftAnswer = randomNumber - randomNumberVariables[1];
-    rightAnswer = randomNumber + randomNumberVariables[1];
-
-    gridNumbers = document.getElementsByClassName('gridItem');
-    Array.from(gridNumbers).forEach(function(gridNumber) {
-        let currentNumber = gridNumber.textContent || gridNumber.innerText;
-        if (parseInt(currentNumber) === randomNumber) {
-            gridNumber.style.backgroundColor = "green";
+    gridItems.forEach(gridItem => {
+        if (gridItem.textContent === randomNumber.toString()) {
+            gridItem.style.backgroundColor = "green";
         }
     });
 }
 
+function populateRandomNumbers() {
+    randomNumber = getRandomNumber(11, 100);
+    randomNumberVariables = [getRandomNumber(1, 5), getRandomNumber(6, 10)]
+
+    randomNumberLabel.value = randomNumber;
+
+    inputBoxes.forEach((inputBox, index) => {
+        let i = (index % 3 === 0) ? 1 : 0;
+        const symbol = index % 2 === 0 ? '+' : '-';
+        inputBox.value = '';
+
+        inputBox.placeholder = `${symbol}${randomNumberVariables[i]}`;
+    });
+
+    gridColors();
+}
+
 document.getElementById('checkButton').addEventListener('click', function() {
-    upperInput = parseInt(document.getElementById('upperInputBox').value);
-    lowerInput = parseInt(document.getElementById('lowerInputBox').value);
-    leftInput = parseInt(document.getElementById('leftInputBox').value);
-    rightInput = parseInt(document.getElementById('rightInputBox').value);
+    for (let index = 0; index < inputBoxes.length; index++) {
+        let inputBox = inputBoxes[index];
+        let i = (index % 3 === 0) ? 1 : 0;
+        const symbol = index % 2 === 0 ? '+' : '-';
+        pointScore = pointScoreLabel.textContent.match(/\d+/g).join();
+        highScore = highScoreLabel.textContent.match(/\d+/g).join();
 
-    if (upperInput === upperAnswer && lowerInput === lowerAnswer && leftInput === leftAnswer && rightAnswer === rightInput) {
-        result++;
-        pointLabel.innerText = "points = " + result;
-
-        generateRandomNumbers();
-    } 
-    else if (document.getElementById('upperInputBox').value === "" || document.getElementById('lowerInputBox').value === "" || document.getElementById('leftInputBox').value === "" || document.getElementById('rightInputBox').value === "") {
-        alert('Udfyld alle tal')
-    } 
-    else {
-        let currentHighScore = document.getElementById('highScoreLabel').innerText;
-        currentHighScore = currentHighScore.charAt(currentHighScore.length - 1);
-
-        if (result > currentHighScore) {
-            document.getElementById('highScoreLabel').innerText = "High Score: " + result;
+        if (inputBox.value === '') {
+            alert('Please fill out all numbers');
+            return;
         }
 
-        result = 0;
-        pointLabel.innerText = "points = " + result;
+        if (inputBox.value !== eval(`${randomNumber} ${symbol} ${randomNumberVariables[i]}`).toString()) {
+            if (pointScore > highScore) {
+                highScoreLabel.textContent = `High Score: ${pointScore}`;
+            }
 
-        gridNumbers = document.getElementsByClassName('gridItem');
-        Array.from(gridNumbers).forEach(function(gridNumber) {
-            let currentNumber = gridNumber.textContent || gridNumber.innerText;
-            gridNumber.style.backgroundColor = "#f0f0f0";
-        });
-        
-        generateRandomNumbers();
+            pointScore = 0;
+            pointScoreLabel.textContent = `Points = ${pointScore}`
+
+            gridColors(true);
+            populateRandomNumbers();
+            return;
+        }
     }
+
+    pointScore++;
+    pointScoreLabel.textContent = `Points = ${pointScore}`;
+    populateRandomNumbers();
 });
 
 Array.from(inputBoxes).forEach(function(inputBox) {
@@ -87,12 +94,5 @@ Array.from(inputBoxes).forEach(function(inputBox) {
     });
 });
 
-const grid = document.getElementById("grid");
-for (let i = 1; i <= 100; i++) {
-    const div = document.createElement("div");
-    div.classList.add("gridItem");
-    div.textContent = i;
-    grid.appendChild(div);
-}
-
-generateRandomNumbers();
+populateGrid();
+populateRandomNumbers();
